fb7574969b5157a755c52ef8510d0155fecf2407
Coloride <108619637+realcoloride@users.noreply.github.com>
2024-04-12 09:25:58 +0200

    Remove useless garbage
    Creating stream for self is already managed when making the previous stream (even the one for yourself)

--- VoiceChatManager.cs
+++ VoiceChatManager.cs
@@ -56,11 +56,11 @@ public partial class VoiceChatManager : Node
 		};
 		ConnectButton.Pressed += () => {
 			Error status = Peer.CreateClient(IPTextBox.Text, 19994);
-            if (status != Error.Ok)
-            {
+            		if (status != Error.Ok)
+			{
 				SetStatus("Could not connect " + status);
-                return;
-            }
+			return;
+            		}
 			
 			Multiplayer.MultiplayerPeer = Peer;
 			SetStatus("Connecting to server " + IPTextBox.Text + ":" + 19994);
@@ -98,8 +98,6 @@ public partial class VoiceChatManager : Node
 		// create streams / playbacks
 		Multiplayer.PeerConnected += (id) => {
 			CreateStreamPlayer(id);
-			// create even one for yourself
-			CreateStreamPlayer(Peer.GetUniqueId());
 		};
 
 		Multiplayer.PeerDisconnected += (id) => {
